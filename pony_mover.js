e6a6765193f7f7494377ec20fdaea90b77fe30f9
Daniel Benamy <daniel@benamy.info>
2014-06-11 01:07:00 -0400

    Converted to the command pattern.

--- pony_mover.js
+++ pony_mover.js
@@ -1,53 +1,81 @@
-function PonyMover($pony, $log) {
-  this.$pony = $pony
-  this.$log = $log
-
-  // Stack of all the moves. Eg.: ['left', 'up', 'down']
-  this.moves = []
-}
-
-PonyMover.prototype.moveDirection = function(direction) {
-  switch (direction) {
-    case 'up':
-      this.$pony.animate({'top': '-=30px'})
-      break
-    case 'down':
-      this.$pony.animate({'top': '+=30px'})
-      break
-    case 'left':
-      this.$pony.animate({'left': '-=30px'})
-      break
-    case 'right':
-      this.$pony.animate({'left': '+=30px'})
-      break
+AbstractCommand = {
+  _execute: function($pony, $log) {
+    this.$pony = $pony;
+    this.$pony.animate(this.doAnimation);
+    this.$el = $('<li>' + this.name + '</li>');
+    $log.append(this.$el);
+  },
+  undo: function() {
+    this.$pony.animate(this.undoAnimation);
+    this.$el.remove();
+    this.$el = null;
   }
+};
+
+function LeftCommand($pony, $log) {
+  this.name = 'left';
+  this.doAnimation = {'left': '-=30px'};
+  this.undoAnimation = {'left': '+=30px'};
+  this._execute.call(this, $pony, $log);
 }
+LeftCommand.prototype = Object.create(AbstractCommand);
+LeftCommand.prototype.constructor = LeftCommand;
+
+function RightCommand($pony, $log) {
+  this.name = 'right';
+  this.doAnimation = {'left': '+=30px'};
+  this.undoAnimation = {'left': '-=30px'};
+  this._execute.call(this, $pony, $log);
+}
+RightCommand.prototype = Object.create(AbstractCommand);
+RightCommand.prototype.constructor = RightCommand;
+
+function DownCommand($pony, $log) {
+  this.name = 'down';
+  this.doAnimation = {'top': '+=30px'};
+  this.undoAnimation = {'top': '-=30px'};
+  this._execute.call(this, $pony, $log);
+}
+DownCommand.prototype = Object.create(AbstractCommand);
+DownCommand.prototype.constructor = DownCommand;
+
+function UpCommand($pony, $log) {
+  this.name = 'up';
+  this.doAnimation = {'top': '-=30px'};
+  this.undoAnimation = {'top': '+=30px'};
+  this._execute.call(this, $pony, $log);
+}
+UpCommand.prototype = Object.create(AbstractCommand);
+UpCommand.prototype.constructor = UpCommand;
+
+
+function PonyMover($pony, $log) {
+  this.$pony = $pony;
+  this.$log = $log;
+
+  // Stack of all the commands.
+  this.commands = [];
+}
+
+PonyMover.prototype.directionToCommandConstructor = {
+  'up': UpCommand,
+  'down': DownCommand,
+  'left': LeftCommand,
+  'right': RightCommand
+};
 
 PonyMover.prototype.move = function(keyCode) {
-  var direction = keyCodeToName[keyCode] // Convert key code to direction name
-
+  var direction = keyCodeToName[keyCode];
   if (direction) {
-    this.moveDirection(direction)
-    this.moves.push(direction)
-    this.$log.append('<li>' + direction + '</li>')
+    var CmdCtor = this.directionToCommandConstructor[direction];
+    var cmd = new CmdCtor(this.$pony, this.$log);
+    this.commands.push(cmd);
   }
-}
-
-PonyMover.prototype.oppositeDirections = {
-  'up': 'down',
-  'down': 'up',
-  'left': 'right',
-  'right': 'left'
-}
+};
 
 PonyMover.prototype.undo = function() {
-  // Get the last move
-  var direction = this.moves.pop()
-
-  if (direction) {
-    var oppositeDirection = this.oppositeDirections[direction]
-
-    this.moveDirection(oppositeDirection)
-    this.$log.find('li:last').remove()
+  var cmd = this.commands.pop();
+  if (cmd) {
+    cmd.undo();
   }
-}
+};
